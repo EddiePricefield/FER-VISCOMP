93390ddb0e998a922eb175dafe5bd2b63c93150c
Eduardo Santana <eduardofsantana17@gmail.com>
2026-05-25 02:25:59 -0300

    GUI - Tela Webcam #3
    Implementei um botão para reiniciar todo o sistema de detecção (é útil em diversas situações) e implementei um gráfico mostrando a variação nas emoções detectadas usando o pyqtgraph

--- kaomotion.py
+++ kaomotion.py
@@ -8,15 +8,17 @@ os.environ["OPENCV_LOG_LEVEL"] = "SILENT"
 import sys
 import cv2
 import numpy
+import pyqtgraph
 import onnxruntime
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QDialog, QColorDialog
 from PyQt6.QtCore import QTimer
-from PyQt6.QtGui import QImage, QPixmap, QResizeEvent
+from PyQt6.QtGui import QImage, QPixmap, QColor
 from PyQt6 import uic
 from enum import Enum, auto
 from pathlib import Path
 
 compressao = 0
+color = QColor(0, 0, 255)
 
 class JanelaCompressao(QDialog):
 
@@ -274,13 +276,23 @@ class TelaWebcam(QMainWindow):
         super().__init__()
         uic.loadUi('UIs/tela_webcam.ui', self)
 
-        self.spinLargura.valueChanged.connect(self.alterar_tamanho_cam)
-        self.spinAltura.valueChanged.connect(self.alterar_tamanho_cam)
-
         self.tela_inicial = tela_inicial
         self.iniciar_deteccao = True
 
+        self.spinLargura.valueChanged.connect(self.alterar_tamanho_cam)
+        self.spinAltura.valueChanged.connect(self.alterar_tamanho_cam)
+        self.btnAlterarCores.clicked.connect(self.alterar_cores)
+        self.btnReset.clicked.connect(self.reiniciar)
+
         self.btnVoltarTela.clicked.connect(self.voltar)
+
+    def alterar_cores(self):
+        global color
+        cor_selecionada = QColorDialog.getColor()
+
+        if cor_selecionada.isValid():
+            color = cor_selecionada
+            self.barras_emocao.setOpts(brush=color.name())
 
     def iniciar_cam(self):
         if not self.iniciar_deteccao:
@@ -308,14 +320,46 @@ class TelaWebcam(QMainWindow):
         self.input_name = self.session.get_inputs()[0].name
         self.emocoes = ['Neutro', 'Feliz', 'Surpreso', 'Triste', 'Bravo', 'Nojo', 'Medo', 'Deboche']
 
+        ## Gráfico de Emoções ##
+        self.graficoEmocoes.clear()
+        self.graficoEmocoes.setBackground('#1E1E1E')            # Cinza
+
+        self.graficoEmocoes.setXRange(0, 100)
+        self.graficoEmocoes.showGrid(x=False, y=False)
+        self.graficoEmocoes.hideAxis('bottom')                  # Remover os valores de 0 a 100 do X
+
+        self.graficoEmocoes.setMouseEnabled(x=False, y=False)
+
+        ## Configuração dos Eixos ##
+        y_dict = dict(enumerate(self.emocoes))
+        eixo_y = self.graficoEmocoes.getAxis('left')
+        eixo_y.setTicks([y_dict.items()])
+        eixo_y.setStyle(showValues=True)
+        eixo_y.setTextPen('w')                                  # Cor do Texto
+        eixo_y.setPen(pyqtgraph.mkPen(color='#1E1E1E'))         # Cor da Linha (tá a mesma do fundo)
+        eixo_y.setWidth(65)                                     # Distância da Borda
+        self.graficoEmocoes.invertY(True)                       # Fazer o "Neutro" ficar no topo do Eixo
+
+        ## Configurações da Própria Barra ##
+        self.barras_emocao = pyqtgraph.BarGraphItem(
+            x0 = 0,
+            y=list(range(len(self.emocoes))),
+            width=numpy.zeros(len(self.emocoes)),
+            height=0.5,
+            brush=color.name(),
+            pen = pyqtgraph.mkPen(color='w', width=1)
+        )
+        self.graficoEmocoes.addItem(self.barras_emocao)
+
         self.img_erro = cv2.imread('imagens/error.png')
         self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
         ## Timer ##
         self.timer = QTimer()
         self.fps = self.cam.get(cv2.CAP_PROP_FPS)
-        self.timer.timeout.connect(self.atualizar_frame)
-        self.timer.start(int(1000 / self.fps))
+        if self.fps != 0:
+            self.timer.timeout.connect(self.atualizar_frame)
+            self.timer.start(int(1000 / self.fps))
 
     def atualizar_frame(self):
         if self.cam is None or not self.cam.isOpened():
@@ -350,7 +394,7 @@ class TelaWebcam(QMainWindow):
 
                 ## Desenhar o Retângulo ##
                 if self.checkRetangulo.isChecked():
-                    cv2.rectangle(frame, (x, y), (x2, y2), (255, 0, 0), 2)
+                    cv2.rectangle(frame, (x, y), (x2, y2), ((color.blue(), color.green(), color.red())), 2)
 
                 face = gray_clahe[y:y2, x:x2]
                 face_resized = cv2.resize(face, (64, 64))
@@ -359,9 +403,15 @@ class TelaWebcam(QMainWindow):
                 predicoes = self.session.run(None, {self.input_name: face_processed})
                 emocao_detectada = self.emocoes[numpy.argmax(predicoes[0])]
 
+                ## Mudar o Gráfico em Tempo Real ##
+                exp_preds = numpy.exp(predicoes[0].flatten() - numpy.max(predicoes[0].flatten()))
+                self.barras_emocao.setOpts(width=(exp_preds / exp_preds.sum()) * 100)
+
                 ## Desenhar a Emoção Acima do Retângulo ##
                 if self.checkTexto.isChecked():
-                    cv2.putText(frame, emocao_detectada, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+                    cv2.putText(frame, emocao_detectada, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, ((color.blue(), color.green(), color.red())), 2)
+        else:
+            self.barras_emocao.setOpts(width=0)
 
         ## Conversão do PixMap para exibir no QLabel ##
         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB).astype(numpy.uint8)
@@ -414,6 +464,16 @@ class TelaWebcam(QMainWindow):
 
     def alterar_tamanho_cam(self):
         self.viewCamera.setFixedSize(self.spinLargura.value(), self.spinAltura.value())
+
+    def reiniciar(self):
+        self.timer.stop()
+        self.detector_rostos = None
+        self.session = None
+
+        if self.cam is not None:
+            self.cam.release()
+
+        self.iniciar_cam()
 
     def voltar(self):
         self.iniciar_deteccao = False
